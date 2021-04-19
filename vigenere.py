cbcb55253befffc39ebcd6257ccd6cae2195bd44
mskwyditd <slowik.tomasz@wp.pl>
2021-04-19 23:11:02 +0200

    added getting keyLen with Kasinski method

--- vigenere.py
+++ vigenere.py
@@ -1,7 +1,17 @@
 from statistics import mean
 
 
+twoletters = [ 'cz', 'ch', 'dz', 'rz', 'sz', 'ci', 'ni', 'zi', 'si' ]
+
+threeletters = ['dzi']
+
+
 ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ '
+
+# dodane prawdopodobieństwa z polskich znaków
+# polishLetterPossibilities = { 'A':9.9, 'B':1.47, 'C':3.96, 'D':3.25, 'E':8.77, 'F':0.3, 'G':1.42,
+# 'H':1.08, 'I':8.21, 'J':2.28, 'K':3.51, 'L':3.92, 'M':2.8, 'N':5.72, 'O':8.6, 'P':3.13, 'Q':0.14,
+# 'R':4.69, 'S':4.98, 'T':3.98, 'U':2.5, 'V':0.04, 'W':4.65, 'X':0.02, 'Y':3.76, 'Z':6.53,  ' ':5. }
 
 polishLetterPossibilities = { 'A':8.91, 'B':1.47, 'C':3.96, 'D':3.25, 'E':7.66, 'F':0.3, 'G':1.42,
 'H':1.08, 'I':8.21, 'J':2.28, 'K':3.51, 'L':2.1, 'M':2.8, 'N':5.52, 'O':7.75, 'P':3.13, 'Q':0.14,
@@ -21,14 +31,13 @@ def encrypt(key, message):
     return ciphertext
 
 def decrypt(key, ciphertext):
-    newKey = ''
-    alphabetLen = len(ALPHABET)
-    for i in key:
+    message = ''
+    for i in range(len(ciphertext)):
         try:
-            newKey += nr_to_letter[ alphabetLen - letter_to_nr[i] -1 ]
+            message += nr_to_letter[ (  letter_to_nr[ciphertext[i]] - letter_to_nr[key[i % len(key)]] ) % len(ALPHABET) ]
         except KeyError:
             print('Letter not in dictionary!')
-    return encrypt(newKey, ciphertext)
+    return message
 
 def findTextProbability(text):
     letters_to_amount = dict(zip(ALPHABET, [0 for i in range(len(ALPHABET))]))
@@ -139,16 +148,101 @@ def readFile(fileName = 'Vigenere.txt'):
     return text
 
 
+def avgDistance(ciphertext, word):
+    length = len(word)
+    positions = []
+    for i in range(len(ciphertext) - length + 1):
+        if ciphertext[i:i+length] == word:
+            positions.append(i)
+
+    gaps = []
+    for i in range(len(positions) - 1):
+        gaps.append( positions[i+1] - positions[i] )
+    if len(positions) <= 1:
+        return -1
+    positions.clear()
+    return sum(gaps)/len(gaps)
+
+
+def findKeyLength_Kasiski(ciphertext, langLetterPossibilities):
+    double = {}
+    tripple = {}
+    for i in range(len(ciphertext) - 2):
+        if ciphertext[i:i+2] in double:
+            double[ciphertext[i:i+2]] += 1
+        else:
+            double[ciphertext[i:i+2]] = 1
+        if ciphertext[i:i+3] in tripple:
+            tripple[ciphertext[i:i+3]] += 1
+        else:
+            tripple[ciphertext[i:i+3]] = 1
+    reversedDouble = [[value, key] for (key, value) in double.items()]
+    reversedTripple = [[value, key] for (key, value) in tripple.items()]
+    reversedDouble.sort(reverse=True)
+    reversedTripple.sort(reverse=True)
+    del reversedDouble[6:]
+    del reversedTripple[3:]
+    possibleKeys = []
+    for i in reversedDouble:
+        possibleKeys.append( avgDistance(ciphertext, i[1]) )
+    for i in reversedTripple:
+        possibleKeys.append( avgDistance(ciphertext, i[1]) )
+    return possibleKeys
+
+
+def isPolish(text):
+    notPolish = 'XQV'
+    for letter in text:
+        if letter in notPolish:
+            return False
+    return True
+
+
+
+
+def bruteForceCaesar(ciphertext):
+    for i in range(len(ALPHABET)):
+        decrypted = decrypt(ALPHABET[i], ciphertext)
+        if isPolish(decrypted):
+            print(i, ": ", decrypted[:10])
+    writing = int(input("Który tekst najbardziej przypomina wiadomość w języku polskim: "))
+    return ALPHABET[writing]
+
+
 if __name__ == '__main__':
-    print('Czytanie pliku: ')
+    print('Czytanie pliku...')
     ciphertext = readFile()
     print('Deszyfrowanie...')
-    keyLength = findKeyLength_Friedman(ciphertext, 0.9 )
-    print('Odzyskana długość klucza: ', keyLength)
-    key = findKey(keyLength, ciphertext, polishLetterPossibilities)
-    print('Odzyskany klucz: ', key)
-    message = decrypt(key, ciphertext)
-    print('Odszyfrowana wiadomość: ', message)
+
+    # keyLength = findKeyLength_Friedman(ciphertext, 0.9 )
+    # print('Odzyskana długość klucza: ', keyLength)
+    # key = findKey(keyLength, ciphertext, polishLetterPossibilities)
+    # print('Odzyskany klucz: ', key)
+    # message = decrypt(key, ciphertext)
+    # print('Odszyfrowana wiadomość: ', message)
+    
+    print("Możliwe, że długość klucza jest równa NWD większości z tych liczb:")
+    print(findKeyLength_Kasiski(ciphertext, polishLetterPossibilities))
+    
+
+    possibleKeyLength = int(input("Jaki długi jest według Ciebie klucz: "))
+
+    possibleKey = ''
+    ciphertexts = ['' for i in range(possibleKeyLength)]
+    for i in range(len(ciphertext)):
+        ciphertexts[i%possibleKeyLength] += ciphertext[i]
+        
+    for text in ciphertexts:
+        possibleKey += bruteForceCaesar(text)
+    
+    print("Możliwy klucz: ", possibleKey)
+    mostPossibleKey = input("Wprowadź klucz: ").upper()
+
+    print('Szyfrogram:')
+    print(encrypt(possibleKey, ciphertext))
+
+    
+
 
     # out = open('resultsOfVigenere.txt', 'wt')
     # out.write(message)
